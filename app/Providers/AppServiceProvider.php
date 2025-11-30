6df0e3cb91b41e1975969277a0d7b9dab05f9e63
nayemspecial <nayemspecial@gmail.com>
2025-12-01 02:39:09 +0600

    Force HTTPS in Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // যদি প্রোডাকশন হয়, তবে HTTPS ফোর্স করবে
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
